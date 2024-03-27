e275e73c8e3cc588a40136fff8050e3dc73706d8
Soham <141822047+XohamOG@users.noreply.github.com>
2024-03-27 23:59:08 +0530

    before 12..........

--- Main.java
+++ Main.java
@@ -1,188 +1,184 @@
 import Model.Clients.*;
+import Model.Records.Records;
 
+import java.util.ArrayList;
 import java.util.InputMismatchException;
+import java.util.List;
 import java.util.Scanner;
 
-/**
- * Identification comments:
- *   Name: soham patil
- *   System: Health Monitoring System
- *   Experiment Title:
- *   Experiment Date:
- *   @version 1.0
- *
- * ....
- * Beginning comments:
- * Filename: Main.java
- * @author:  soham patil
- * Overview: This is the main class used to create objects for Client Class, Records class; Menu Driven Program. In this file we have achieved the following
- * - Object Creation
- * - Calling Setters and Getters
- * - Creating Object Arrays
- * - Looping through Object Array
- * - Displaying contents of object array
- *
- */
-
 public class Main {
-    public static void main(String[] args)
-    {
-        int n;
-
-        //Creating a Scanner Object to take input
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
-        //Creating a single Object
-        Client c1 = new Client();
-
-        // Using setters to set the values
+        Client[] clientArr = new Client[3];
+        int ch;
+        List<Records> recordsList = new ArrayList<>();
 
 
-        //Display client data
-        c1.display();
-
-        //Creating client object array
-        Client clientArr[] = new Client[3];
-
-        //Using For Loop to Take user input for each client object
-        do{
-
-           
+        do {
             System.out.println("1 - Add client");
             System.out.println("2 - Edit client");
             System.out.println("3 - Delete client");
             System.out.println("4 - Display clients");
             System.out.println("5 - Enter record");
-            System.out.println("6 - Exit");
+            System.out.println("6 - Display records");
+            System.out.println("7 - Exit");
             System.out.print("Enter Choice of Operation: ");
 
-            int ch;
             ch = sc.nextInt();
+            sc.nextLine(); // Consume newline character
 
-            if (ch == 1) {
-                
-                try {
-                    
-                
+            switch (ch) {
+                case 1:
+                        try {
+                         for (int i = 0; i < clientArr.length; i++) {
+                            System.out.println("Enter no. of clients to be entered:");
+                            int n = sc.nextInt();
+                            sc.nextLine(); // Consume newline character
+                            for (int j = 1; j <= n; j++) {
+                                System.out.println("Enter Client Name: ");
+                                String name = sc.nextLine();
+                                System.out.println("Enter Client Age: ");
+                                int age = sc.nextInt();
+                                sc.nextLine(); // Consume newline character
+                                System.out.println("Enter Client Email: ");
+                                String email = sc.nextLine();
+                                System.out.println("Enter Client Gender: ");
+                                String gender = sc.nextLine();
+                                clientArr[i] = new Client(email, name, age, gender);
+                                
+                            }
+                            break;
+                        }
+                     }   catch (InputMismatchException e) {
+                        System.out.println("Please write a valid integer input");
+                     }
+                        break;
+
+                case 2:
+                    try {
+                    System.out.println("Existing Clients:");
                     for (int i = 0; i < clientArr.length; i++) {
-                        sc.nextLine();
-    
-                        System.out.println("Enter no. of clients to be entered:");
-                         
-                         //Creating a Scanner Object to take input
-                         Scanner sc1 = new Scanner(System.in);
-                         n = sc1.nextInt();
-    
-                         for (i=1; i<=n; i++)
-                        {
-    
-                        System.out.println("Enter Client Name: ");
+                        System.out.println((i + 1) + ". " + clientArr[i].getf_name());
+                    }
+
+                    System.out.println("Enter the client number you want to edit:");
+                    int userChoice = sc.nextInt();
+                    sc.nextLine(); // Consume newline character
+
+                    if (userChoice > 0 && userChoice <= clientArr.length) {
+                        int index = userChoice - 1;
+                        Client selectedClient = clientArr[index];
+
+                        System.out.println("Selected Client Information:");
+                        selectedClient.display();
+
+                        System.out.println("Enter new information for the selected client:");
+                        System.out.print("Enter Client Name: ");
                         String name = sc.nextLine();
-    
-                        System.out.println("Enter Client Age: ");
+                        selectedClient.setf_name(name);
+
+                        System.out.print("Enter Client Age: ");
                         int age = sc.nextInt();
-                        sc.nextLine();
-    
-                        System.out.println("Enter Client Email: ");
+                        selectedClient.setf_age(age);
+                        sc.nextLine(); // Consume newline character
+
+                        System.out.print("Enter Client Email: ");
                         String email = sc.nextLine();
-    
-                        System.out.println("Enter Client Gender: ");
+                        selectedClient.setClient_email(email);
+
+                        System.out.print("Enter Client Gender: ");
                         String gender = sc.nextLine();
-    
-                        clientArr[i] = new Client(email, name, age, gender);
-                        } 
+                        selectedClient.setf_gender(gender);
+
+                        System.out.println("Updated Client Information:");
+                        selectedClient.display();
+                    } else {
+                        System.out.println("Invalid client number.");
                     }
+                    break;
+                } catch (Exception e) {
+                    System.out.println();
                 }
-    
-                 catch (InputMismatchException e) {
+
+                case 3:
+                    for (int i = 0; i < clientArr.length; i++) {
+                        System.out.print("Enter Index of client to delete : ");
+                        int user = sc.nextInt();
+                        sc.nextLine(); // Consume newline character
+                        clientArr[user].setf_name(null);
+                        clientArr[user].setf_age(0);
+                        clientArr[user].setClient_email(null);
+                        clientArr[user].setf_gender(null);
+                    }
+                    break;
+
+                case 4:
+                    try{
+
+                        System.out.println("existing clients are ");
+                        System.out.println();
+                         for (int i = 0; i < clientArr.length; i++) {
+                        clientArr[i].display();}
+                        System.out.println();
+
+                }
+                    catch (NullPointerException e) {
+                        
+                    }
+
+                    break;
+
+                case 5:
+
+                try{
+
+                    System.out.println("existing clients are ");
+                    System.out.println();
+                     for (int i = 0; i < clientArr.length; i++) {
+                    clientArr[i].display();}
                     System.out.println();
 
-                    System.out.println("Please write an valid integer input");
-                    
-                    System.out.println();
-                    }
-            } else if (ch == 2) {
-
-                // Displaying existing clients for selection
-                System.out.println("Existing Clients:");
-                for (int i = 0; i < clientArr.length; i++) {
-                    System.out.println((i + 1) + ". " + clientArr[i].getf_name());
-                }
-
-                // Prompting user to select a client to edit
-                System.out.println("Enter the client number you want to edit:");
-                int userChoice = sc.nextInt();
-                sc.nextLine();
-
-                if (userChoice > 0 && userChoice <= clientArr.length) {
-                    int index = userChoice - 1;
-                    Client selectedClient = clientArr[index];
-
-                    System.out.println("Selected Client Information:");
-                    selectedClient.display();
-
-                    System.out.println("Enter new information for the selected client:");
-                    System.out.print("Enter Client Name: ");
-                    String name = sc.nextLine();
-                    selectedClient.setf_name(name);
-
-                    System.out.print("Enter Client Age: ");
-                    int age = sc.nextInt();
-                    selectedClient.setf_age(age);
-                    sc.nextLine();
-
-                    System.out.print("Enter Client Email: ");
-                    String email = sc.nextLine();
-                    selectedClient.setClient_email(email);
-
-                    System.out.print("Enter Client Gender: ");
-                    String gender = sc.nextLine();
-                    selectedClient.setf_gender(gender);
-
-                    System.out.println("Updated Client Information:");
-                    selectedClient.display();
-                } else {
-                    System.out.println("Invalid client number.");
-                }
-            } else if (ch == 3) {
-                for (int i = 0; i < clientArr.length; i++) {
-                    System.out.print("Enter Index of client to delete : ");
-                    int user = sc.nextInt();
-                    sc.nextLine();
-                    clientArr[user].setf_name(null);
-                    clientArr[user].setf_age(0);
-                    clientArr[user].setClient_email(null);
-                    clientArr[user].setf_gender(null);
-                }
-            } else if (ch == 4) {
-                for (int i = 1; i < clientArr.length; i++) {
-                    System.out.print("");
-                    clientArr[i].display();
-                }
             }
-                else if (ch == 5){
-                    // Displaying existing clients for selection
-                System.out.println("Existing Clients:");
-                for (int i = 0; i < clientArr.length; i++) {
-                    System.out.println((i + 1) + ". " + clientArr[i].getf_name());
+                catch (NullPointerException e) {
+                    
                 }
-
-                // Prompting user to select a client to enter records
-                System.out.println("Enter the client number to enter records:");
-                int userChoice = sc.nextInt();
+                System.out.println("Enter Client Id: ");
+                int client_id = sc.nextInt();
+                // To avoid unnecessary skip of input
                 sc.nextLine();
-   
+                System.out.println("Enter number of steps: ");
+                int steps = sc.nextInt();
+                System.out.println("Enter sleep hours: ");
+                int sleephrs = sc.nextInt();
 
+                System.out.println("Enter litres of water consumed: ");
+                int hydration = sc.nextInt();
+
+                System.out.println("Meditation (true/false): ");
+                boolean meditation = sc.nextBoolean();
+                sc.nextLine();
+
+                recordsList.add(new Records(steps, sleephrs, hydration, meditation));
                 
-                }
-             else if (ch == 6) {
-                return;
-            } else {
+                break;
+
+                case 6:
+                    for (Records record : recordsList) {
+                    // Print out the details of each Records object
+                    System.out.println("Steps: " + record.getsteps());
+                    System.out.println("Sleep Hours: " + record.getsleephrs());
+                    System.out.println("Water Consumed: " + record.gethydration());
+                    System.out.println("Meditation: " + record.getmeditation());
+                    System.out.println();
+                    }
+                    break;
+                case 7:
+                    return;
+                default:
                 System.out.println("Invalid input.");
             }
-
         }
-
-         while (true);
-}
+        
+    while (true);
+    }
 }
